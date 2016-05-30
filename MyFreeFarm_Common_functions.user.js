56c2fec66afb9fcb2a3318b7a8280c656beb7d2f
linus--tux <hemingway--ernest@outlook.com>
2016-05-30 19:29:06 +0200

    Improvements: animal treatment and factory

--- MyFreeFarm_Common_functions.user.js
+++ MyFreeFarm_Common_functions.user.js
@@ -3,12 +3,12 @@
 // @namespace      https://github.com/BastianKanaan/GMscripts_MyFreeFarm
 // @author         BastianKanaan
 // @description    Common functions for MyFreeFarm-Scripts
-// @date           11.07.2015
-// @version        2.1.7
+// @date           25.05.2016
+// @version        2.1.8
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
-const VERSIONfunctionFile = "2.1.7";
+const VERSIONfunctionFile = "2.1.8";
 var DEVMODE=GM_getValue("devmode",false);
 var DEVMODE_EVENTS=GM_getValue("devmode_events",false);
 var DEVMODE_FUNCTION=GM_getValue("devmode_function",false);
